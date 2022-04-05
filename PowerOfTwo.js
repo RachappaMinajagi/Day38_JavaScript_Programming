5fbe1221f3c35ac71a144e72c152086e459d65ad
RachappaMinajagi <59776797+RachappaMinajagi@users.noreply.github.com>
2022-04-05 21:54:16 +0530

    Delete PowerOfTwo.js

--- PowerOfTwo.js
+++
@@ -1,8 +0,0 @@
-//Reading input from console using prompt-sync NodeJS Module
-const prompt = require("prompt-sync")()
-const n = prompt("Enter the value for N: ");
-let baseNumber = 2;
-
-for(let i =0 ; i<= n; i++){
-    console.log(`2^${i}   : ${Math.pow(baseNumber, i)}`)
-}
